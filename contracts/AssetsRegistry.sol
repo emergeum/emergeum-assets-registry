151c54b46d618cc2b03d40d769233b9b9528037e
Maksim Tolochko <maksim.tolo@gmail.com>
2019-10-26 23:18:33 +0300

    Fix contracts

--- contracts/AssetsRegistry.sol
+++ contracts/AssetsRegistry.sol
@@ -4,65 +4,69 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract AssetsRegistry is Ownable {
     /**
-     * @dev Key is a ticker, values is a destination address
+     * @dev Key is a ticker, values is a backup address
      */
     mapping(bytes32 => bytes32) public assets;
 
     bytes32[] public tickersList;
 
-    constructor(bytes32[] memory tickers, bytes32[] memory reserveAddresses) public {
-        require(tickers.length == reserveAddresses.length, "Tickers length must be equal to addresses length");
+    constructor(bytes32[] memory tickers, bytes32[] memory backupAddresses) public {
+        require(tickers.length == backupAddresses.length, "Tickers length must be equal to addresses length");
 
         for (uint i = 0; i < tickers.length; i++) {
-            require(tickers[i] != 0x0, "Ticker can't be empty");
-            require(reserveAddresses[i] != 0x0, "Address can't be empty");
+            require(!isEmpty(tickers[i]), "Ticker can't be empty");
+            require(!isEmpty(backupAddresses[i]), "Address can't be empty");
 
-            assets[tickers[i]] = reserveAddresses[i];
+            assets[tickers[i]] = backupAddresses[i];
             tickersList.push(tickers[i]);
         }
+    }
+
+    function isEmpty(bytes32 val) internal pure returns(bool) {
+        return val[0] == 0;
     }
 
     function getTickers() external view returns(bytes32[] memory) {
         return tickersList;
     }
 
-    function getReserveAddresses() external view returns(bytes32[] memory reserveAddresses) {
-        reserveAddresses = new bytes32[](tickersList.length);
+    function getBackupAddresses() external view returns(bytes32[] memory backupAddresses) {
+        backupAddresses = new bytes32[](tickersList.length);
 
         for (uint i = 0; i < tickersList.length; i++) {
-            reserveAddresses[i] = assets[tickersList[i]];
+            backupAddresses[i] = assets[tickersList[i]];
         }
 
-        return reserveAddresses;
+        return backupAddresses;
     }
 
     function getTickersLength() external view returns(uint256) {
         return tickersList.length;
     }
 
-    function setAsset(bytes32 ticker, bytes32 reserveAddress) external onlyOwner {
-        require(ticker != 0x0, "Ticker can't be empty");
-        require(reserveAddress != 0x0, "Address can't be empty");
+    function setAsset(bytes32 ticker, bytes32 backupAddress) external onlyOwner {
+        require(!isEmpty(ticker), "Ticker can't be empty");
+        require(!isEmpty(backupAddress), "Address can't be empty");
 
         if (hasTicker(ticker)) {
             removeFromArray(ticker);
         }
 
-        assets[ticker] = reserveAddress;
+        assets[ticker] = backupAddress;
         tickersList.push(ticker);
     }
 
     function deleteAsset(bytes32 ticker) external onlyOwner {
-        require(ticker != 0x0, "Ticker can't be empty");
+        require(!isEmpty(ticker), "Ticker can't be empty");
         require(hasTicker(ticker), "Ticker doesn't exist");
 
-        assets[ticker] = 0x0;
+        delete assets[ticker];
         removeFromArray(ticker);
     }
 
     function resetAssets() external onlyOwner {
         for (uint i = 0; i < tickersList.length; i++) {
-            assets[tickersList[i]] = 0x0;
+            delete assets[tickersList[i]];
         }
 
         tickersList.length = 0;
@@ -77,7 +81,7 @@ contract AssetsRegistry is Ownable {
     }
 
     function hasTicker(bytes32 ticker) internal view returns (bool) {
-        return assets[ticker] != 0x0;
+        return !isEmpty(assets[ticker]);
     }
 
     function getIndexByTicker(bytes32 ticker) internal view returns (uint) {
